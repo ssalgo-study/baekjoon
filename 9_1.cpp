f8233e81230f2989f7ac909dd51d8b6b93ae568b
Boyeon Kim <70560199+by-nny@users.noreply.github.com>
2022-05-08 02:26:04 +0900

    Boyeon

---
+++ 9_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fac(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	else {
+		return n * fac(n - 1);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << fac(n) << endl;
+
+	return 0;
+}
