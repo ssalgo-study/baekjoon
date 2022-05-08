3733e1e4be227b0425bcf609084fdd6c5918ae3a
Boyeon Kim <70560199+by-nny@users.noreply.github.com>
2022-05-08 16:10:51 +0900

    Update 10_1.cpp

--- boyeon/week1/10_1.cpp
+++ boyeon/week1/10_1.cpp
@@ -13,8 +13,8 @@ int main() {
 
 	int sum = 0;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
+	for (int i = 0; i < n-2; i++) {
+		for (int j = i + 1; j < n-1; j++) {
 			for (int k = j + 1; k < n; k++) {
 				if (card[i] + card[j] + card[k] >= sum && card[i] + card[j] + card[k] <= m) //�� sum���� Ŀ�� �� ...?
 					sum = card[i] + card[j] + card[k];
